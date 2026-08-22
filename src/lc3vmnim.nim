18a3886ccde69dcb3b73aaad65db7ebb55aab95d
mhimavamshi <himavamshi1212@gmail.com>
2026-08-22 12:46:30 +0530

    set up the basic loop

--- src/lc3vmnim.nim
+++ src/lc3vmnim.nim
@@ -1,5 +1,59 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import alltypes
+import memoryutils
+import ops
 
-when isMainModule:
-  echo("Hello, World!")
+proc main() =
+  # 2^16 locations (16 bit unsigned integer), and each has 16 bit value
+  var memory: Memory
+
+  # 10 registers, each 16 bits - 8 general purpose, 1 PC, 1 COND flag
+  # const REGISTERSMAX = 10
+  var registers: Registers
+  
+  registers[Register.COND] = FL_ZRO
+
+  const START = 12288
+  registers[Register.PC] = START 
+
+  var running = Running(true);
+
+  while running:
+    let instr = memRead(registers[Register.PC])
+    registers[Register.PC] += 1
+    
+    let op = instr shr 12 # get the first 4 bits from 16 bit word which is the opcode 
+    let code = Opcodes(op)
+
+    case code:
+      of ADD:
+        addOp(instr, registers)
+      of AND:
+        andOp(instr, registers)
+      of NOT:
+        notOp(instr, registers)
+      of BR:
+        br(instr, registers)                                                    
+      of JMP:
+        jmp(instr, registers)
+      of JSR:
+        jsr(instr, registers)
+      of LD:
+        ld(instr, registers)
+      of LDI:
+        ldi(instr, registers)
+      of LDR:
+        ldr(instr, registers)
+      of LEA:
+        lea(instr, registers)
+      of ST:
+        st(instr, registers)
+      of STI:
+        sti(instr, registers)
+      of STR:
+        str(instr, registers)
+      of TRAP:
+        trap(instr, registers, memory, running)
+      of RES, RTI:
+        badOpcode()
+
+main()
