2472ea64e09d787248e12ae88c927c50d9a0c378
mhimavamshi <himavamshi1212@gmail.com>
2026-08-22 12:47:37 +0530

    signextend, updateflags

---
+++ src/utils.nim
@@ -0,0 +1,14 @@
+import alltypes
+
+proc signExtend*(x: uint16, bit_count: int): uint16 = 
+  result = x 
+  if ((x shr (bit_count - 1)) and 1'u16) == 1'u16:
+    result = result or uint16(0xFFFF'u16 shl bit_count)
+
+proc updateFlags*(r: Register, registers: var Registers) =
+  if registers[r] == 0:
+    registers[Register.COND] = FL_ZRO
+  elif (registers[r] shr 15) == 1:
+    registers[Register.COND] = FL_NEG
+  else:
+    registers[Register.COND] = FL_POS                                    
