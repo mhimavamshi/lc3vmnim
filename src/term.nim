88bab702cdb98b6fac63b38abf79b675778e7ab5
mhimavamshi <himavamshi1212@gmail.com>
2026-08-30 11:45:33 +0530

    add term

---
+++ src/term.nim
@@ -0,0 +1,37 @@
+import std/termios
+import std/posix
+
+var originalTio: Termios
+
+proc disableInputBuffering*() =
+    # NOTE: -1 is returned for error
+    discard tcGetAttr(STDIN_FILENO, addr originalTio) 
+
+    var newTio = originalTio
+    newTio.c_lflag = newTio.c_lflag and not (ICANON or ECHO)
+
+    discard tcSetAttr(STDIN_FILENO, TCSANOW, addr newTio)
+
+proc restoreInputBuffering*() =
+    discard tcSetAttr(STDIN_FILENO, TCSANOW, addr originalTio)
+
+#[
+Replace with std/selectors later, maybe
+]#
+proc checkKey*(): bool =
+    var readfds: TFdSet
+    FD_ZERO(readfds)
+    FD_SET(STDIN_FILENO, readfds)
+
+    var timeout = Timeval(
+        tv_sec: Time(0),
+        tv_usec: 0
+    )
+
+    result = select(
+        STDIN_FILENO + 1,
+        addr readfds,
+        nil,
+        nil,
+        addr timeout
+    ) > 0
